2ec27a53d1987987506d64fc119621dd6df0e543
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-03-19 11:05:07 +0530

    theme added

---
+++ src/theme/muiTheme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main:"#fff"
+    },
+    secondary: {
+      main:"#272727",
+    }
+  },
+});
+
+
+export default theme;
