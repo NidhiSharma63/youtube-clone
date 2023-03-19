707aca0081db9c9bfc86e111f92c395927990736
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-03-19 12:39:32 +0530

    drawer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
 import Navbar from "components/Navbar";
+import DrawerComponent from "components/DrawerComponent";
 
 const App: React.FC = (): JSX.Element => {
   const Routes = {
     path: "/",
-    element: <Navbar />,
+    element: <DrawerComponent />,
   };
   const routing = useRoutes([Routes]);
 
