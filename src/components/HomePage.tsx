09a69f488a0ea56a3134c3769812876f8f5af57e
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-03-20 12:43:29 +0530

    useInterval added

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,7 +2,9 @@ import { Grid, Card, Typography, CardContent, CardMedia } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import { IVideo } from "common/Interfaces";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+
+import useInterval from "hooks/useInterval";
 
 interface IVideoProps {
   videoProps: IVideo;
@@ -11,13 +13,89 @@ interface IVideoProps {
 const HomePage = ({ videoProps }: IVideoProps) => {
   const [publishedTime, setPublishedTime] = useState<string>();
 
-  useEffect(() => {
+  // const handleTimeCreate = useCallback(() => {
+  //   const videoPublishedTime = new Date(videoProps.snippet.publishTime);
+  //   const todaysTime = new Date();
+  //   let minutes: number | undefined;
+  //   let hours: number | undefined;
+  //   let days: number | undefined;
+  //   let year: number | undefined;
+  //   let month: number | undefined;
+
+  //   console.log("runs");
+  //   const differenceBetweenTime =
+  //     todaysTime.getTime() - videoPublishedTime.getTime();
+
+  //   // Convert milliseconds to seconds, minutes, hours, or days as necessary
+  //   var seconds = Math.floor(differenceBetweenTime / 1000);
+  //   setPublishedTime(`${seconds} second${seconds > 1 ? "s" : ""}`);
+
+  //   if (seconds >= 60) {
+  //     minutes = Math.floor(differenceBetweenTime / 60000);
+  //     setPublishedTime(`${minutes} minute${minutes > 1 ? "s" : ""}`);
+  //   }
+  //   if (minutes && minutes >= 60) {
+  //     hours = Math.floor(minutes / 60);
+  //     setPublishedTime(`${hours} hour${hours > 1 ? "s" : ""}`);
+  //   }
+  //   if (hours && hours >= 24) {
+  //     days = Math.floor(hours / 24);
+  //     setPublishedTime(`${days} day${days > 1 ? "s" : ""}`);
+  //   }
+
+  //   if (days && days >= 365) {
+  //     year = Math.floor(days / 365);
+  //     setPublishedTime(`${year} year${year > 1 ? "s" : ""}`);
+  //   }
+  // }, [videoProps]);
+
+  // useEffect(() => {
+  //   const interVal = setInterval(() => {
+  //     const videoPublishedTime = new Date(videoProps.snippet.publishTime);
+  //     const todaysTime = new Date();
+  //     let minutes: number | undefined;
+  //     let hours: number | undefined;
+  //     let days: number | undefined;
+  //     let year: number | undefined;
+  //     let month: number | undefined;
+
+  //     const differenceBetweenTime =
+  //       todaysTime.getTime() - videoPublishedTime.getTime();
+
+  //     // Convert milliseconds to seconds, minutes, hours, or days as necessary
+  //     var seconds = Math.floor(differenceBetweenTime / 1000);
+  //     setPublishedTime(`${seconds} second${seconds > 1 ? "s" : ""}`);
+
+  //     if (seconds >= 60) {
+  //       minutes = Math.floor(differenceBetweenTime / 60000);
+  //       setPublishedTime(`${minutes} minute${minutes > 1 ? "s" : ""}`);
+  //     }
+  //     if (minutes && minutes >= 60) {
+  //       hours = Math.floor(minutes / 60);
+  //       setPublishedTime(`${hours} hour${hours > 1 ? "s" : ""}`);
+  //     }
+  //     if (hours && hours >= 24) {
+  //       days = Math.floor(hours / 24);
+  //       setPublishedTime(`${days} day${days > 1 ? "s" : ""}`);
+  //     }
+
+  //     if (days && days >= 365) {
+  //       year = Math.floor(days / 365);
+  //       setPublishedTime(`${year} year${year > 1 ? "s" : ""}`);
+  //     }
+  //   }, 1000);
+  //   return () => clearInterval(interVal);
+  // }, []);
+
+  useInterval(() => {
+    // your code here
     const videoPublishedTime = new Date(videoProps.snippet.publishTime);
     const todaysTime = new Date();
     let minutes: number | undefined;
     let hours: number | undefined;
     let days: number | undefined;
     let year: number | undefined;
+    let month: number | undefined;
 
     const differenceBetweenTime =
       todaysTime.getTime() - videoPublishedTime.getTime();
@@ -43,7 +121,7 @@ const HomePage = ({ videoProps }: IVideoProps) => {
       year = Math.floor(days / 365);
       setPublishedTime(`${year} year${year > 1 ? "s" : ""}`);
     }
-  }, [videoProps]);
+  }, 1000);
 
   return (
     <Grid item lg={3} sm={4} xs={6}>
@@ -80,7 +158,7 @@ const HomePage = ({ videoProps }: IVideoProps) => {
             {<CheckCircleIcon sx={{ fontSize: ".9rem" }} />}
           </Typography>
           <Typography color="secondary.dark" variant="body2">
-            {publishedTime}ago
+            {publishedTime} ago
           </Typography>
         </CardContent>
       </Card>
