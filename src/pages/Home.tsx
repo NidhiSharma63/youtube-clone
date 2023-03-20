55df4a8413e7bfcece3a18d80722bcbf2acbba8c
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-03-20 07:27:18 +0530

    usequery function updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,32 +6,34 @@ import { IVideo } from "common/Interfaces";
 import { useQuery } from "react-query";
 import customAxiosRequest from "constant/customAxiosRequest";
 import { BASE_URL } from "constant/Misc";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const { state } = useContext(searchContext);
   const [search, setSearch] = useState<string>(state.search);
 
-  const { data } = useQuery(
-    ["videos", search],
-    () =>
-      customAxiosRequest(`${BASE_URL}/search?part=snippet&q=${state.search}`),
-    { staleTime: 1000 * 60 * 10000 }
-  );
-  useEffect(() => {
-    setSearch(state.search);
-  }, [state.search]);
+  const navigate = useNavigate();
 
-  console.log(search, "this is data");
+  const { data }: any = useQuery({
+    queryKey: ["videos", search],
+    queryFn: () =>
+      customAxiosRequest(`${BASE_URL}/search?part=snippet&q=${state.search}`),
+    staleTime: 1000 * 60 * 10000,
+  });
+
+  useEffect(() => {
+    if (state.search) {
+      setSearch(state.search);
+      // navigate(`/search/${state.search}`);
+    }
+  }, [state.search, navigate]);
+
   return (
     <>
       <Grid container spacing={1}>
-        {
-          // @ts-ignore
-
-          data?.data?.items?.map((item: IVideo) => {
-            return <HomePage key={item.snippet.title} videoProps={item} />;
-          })
-        }
+        {data?.data?.items?.map((item: IVideo) => {
+          return <HomePage key={item.snippet.description} videoProps={item} />;
+        })}
       </Grid>
     </>
   );
