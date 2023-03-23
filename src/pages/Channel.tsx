fcbc971438aefe0d767facfd6ae341e6629a7312
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-03-23 20:39:08 +0530

    interface added

--- src/pages/Channel.tsx
+++ src/pages/Channel.tsx
@@ -7,11 +7,60 @@ import banner from "images/banner2.jpg";
 import { useState } from "react";
 
 import formatCounts from "utils/formatCounts";
+
+import { ISnippet, IVideo } from "common/Interfaces";
+
+interface IChannelDataSnippte extends ISnippet {
+  localized: {
+    description: string;
+    title: string;
+  };
+  customUrl: string;
+}
+
+interface IChannelData {
+  data?: {
+    brandingSettings: {
+      channel: {
+        country: string;
+        description: string;
+        keywords: string;
+        title: string;
+        trackingAnalyticsAccountId: string;
+        unsubscribedTrailer: string;
+      };
+
+      image: {
+        bannerExternalUrl: string;
+      };
+    };
+    contentDetails: {
+      relatedPlaylists: {
+        like: string;
+        uploads: string;
+      };
+      id: string;
+      kind: string;
+    };
+    snippet: IChannelDataSnippte;
+    statistics: {
+      hiddenSubscriberCount: boolean;
+      subscriberCount: string;
+      videoCount: string;
+      viewCount: string;
+    };
+  }[];
+}
+
+interface IChannelVideData {
+  data?: IVideo[];
+}
+
 const Channel = () => {
   const { id } = useParams();
   const [wordLength, setWordLength] = useState<number>(60);
 
-  const { data: channelData }: any = useQuery({
+  const { data: channelData }: IChannelData = useQuery({
     queryKey: ["channel", id],
     queryFn: () =>
       customAxiosRequest(`${BASE_URL}/channels?part=snippet&id=${id}`),
@@ -21,15 +70,28 @@ const Channel = () => {
     refetchOnMount: false,
   });
 
+  // console.log(channelData, "chnneldata");
+
+  const { data: channelVideo }: IChannelVideData = useQuery({
+    queryKey: ["channelVideo", id],
+    queryFn: () =>
+      customAxiosRequest(`${BASE_URL}/search?part=snippet&channelId=${id}`),
+    staleTime: 1000 * 60 * 10000,
+    select: (suggestedVideo) => suggestedVideo.data.items,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+  });
+
   const hanldeWordLength = (): void => {
-    setWordLength((prev) => {
-      return prev === channelData[0].snippet.localized.description.length
-        ? 60
-        : channelData[0].snippet.localized.description.length;
-    });
+    if (channelData) {
+      setWordLength((prev) => {
+        return prev === channelData[0].snippet.localized.description.length
+          ? 60
+          : channelData[0].snippet.localized.description.length;
+      });
+    }
   };
 
-  console.log(channelData, "hhhhhhh");
   console.log(id, "thid is ");
   return (
     <Grid container>
